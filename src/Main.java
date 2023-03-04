b16e4410308757eb3d2b6eb32f1ada138c881d5c
Radik <radik-i@yandex.ru>
2023-03-04 19:19:00 +0300

    Проверьте ДЗ!

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println ("Название: " + book1.getBookName() + "  Автор: "+ book1.getAuthor().getSurname()+ " " + book1.getAuthor().getName()+ "  год: "+ book1.getPublishingYear());
         System.out.println ("Название: " + book2.getBookName() + "  Автор: "+ book2.getAuthor().getSurname()+ " " + book2.getAuthor().getName() + "  год: "+ book2.getPublishingYear());
 
-        book1.setPublishingYear(1901);
+        book1.setPublishingYear(1905);
 
         System.out.println ("Название: " + book1.getBookName() + "  Автор: "+ book1.getAuthor().getSurname()+ " " + book1.getAuthor().getName()+ "  год: "+ book1.getPublishingYear());
 
