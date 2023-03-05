ea56b49726bd39ef65ab595edd4d15b093e42034
Radik <radik-i@yandex.ru>
2023-03-05 16:25:59 +0300

    Проверьте ДЗ!

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,12 @@ public class Main {
         Book book1 = new Book("Чайка", author1, 1896);
         Book book2 = new Book("Война и мир", author2, 1869);
 
-        System.out.println ("Название: " + book1.getBookName() + "  Автор: "+ book1.getAuthor().getSurname()+ " " + book1.getAuthor().getName()+ "  год: "+ book1.getPublishingYear());
-        System.out.println ("Название: " + book2.getBookName() + "  Автор: "+ book2.getAuthor().getSurname()+ " " + book2.getAuthor().getName() + "  год: "+ book2.getPublishingYear());
+        System.out.println("Название: " + book1.getBookName() + "  Автор: " + book1.getAuthor().getSurname() + " " + book1.getAuthor().getName() + "  год: " + book1.getPublishingYear());
+        System.out.println("Название: " + book2.getBookName() + "  Автор: " + book2.getAuthor().getSurname() + " " + book2.getAuthor().getName() + "  год: " + book2.getPublishingYear());
 
         book1.setPublishingYear(1905);
 
-        System.out.println ("Название: " + book1.getBookName() + "  Автор: "+ book1.getAuthor().getSurname()+ " " + book1.getAuthor().getName()+ "  год: "+ book1.getPublishingYear());
+        System.out.println("Название: " + book1.getBookName() + "  Автор: " + book1.getAuthor().getSurname() + " " + book1.getAuthor().getName() + "  год: " + book1.getPublishingYear());
 
     }
 
